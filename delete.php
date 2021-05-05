6aa6bf387a345b0a40de85d45792d09721b1a03c
Axel Gromat <axel.gromat@gmail.com>
2021-05-05 14:46:43 +0200

    comments

--- delete.php
+++ delete.php
@@ -1,14 +1,23 @@
 <?php require 'inc/header.php';
+// On récupère l'id de l'user à supprimer
 $id = $_POST['id'];
+
+// On récupère le token de Cross Site Request Forgery que l'on a envoyé via le formulaire
 $token = $_POST['csrf_token'];
 
-var_dump($_POST);
-if (isset($_POST['delete']) && $_POST['csrf_token'] == $token) {
+// var_dump($_POST);
+
+//? Deux méthodes (1 bonne, 1 mauvaise, la mauvaise est commentée)
+// if (isset($_POST['delete']) && $_POST['csrf_token'] == $token) {
+//* Je vérifie la soumission de mon formulaire et je vérifie le token csrf du formulaire avec celui que je possède.
+if (isset($_POST['delete']) && hash_equals($token, $_POST['csrf_token'])) {
+    //* Une fois la vérification passée, je prepare une requête SQL de suppression en fonction de l'id de l'user
     try {
         $sth = $connect->prepare("DELETE FROM users WHERE id =:id");
         $sth->bindValue(':id', $id);
 
         $result = $sth->execute();
+        //* Je redirige vers la page précédente si la suppression a eu lieu, ou j'affiche un message autrement. 
         if ($result) {
             header('Location: ' . $_SERVER['HTTP_REFERER']);
         } else {
